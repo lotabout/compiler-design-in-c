3eaab3ee5439f7602fd8ddd3026662c377d080ca
Mark Wallace <lotabout@gmail.com>
2013-12-07 15:15:53 +0800

    NFA: add lexical analyzer.

--- chap02/nfa.c
+++ chap02/nfa.c
@@ -303,3 +303,180 @@ void printmacs()
         ptab(Macros, print_a_macro, NULL, 1);
     }
 }
+
+/*-----------------------------------------------------------------------------
+ * LeX's own lexical analyzer
+ *---------------------------------------------------------------------------*/
+typedef enum {
+    EOS = 1,      /* end of string     */
+    ANY,          /* .                 */
+    AT_BOL,       /* ^                 */
+    AT_EOL,       /* $                 */
+    CCL_END,      /* ]                 */
+    CCL_START,    /* [                 */
+    CLOSE_CURLY,  /* }                 */
+    CLOSE_PAREN,  /* )                 */
+    CLOSURE,      /* *                 */
+    DASH,         /* -                 */
+    END_OF_INPUT, /* EOF               */
+    L,            /* literal character */
+    OPEN_CURLY,   /* {                 */
+    OPEN_PAREN,   /* (                 */
+    OPTIONAL,     /* ?                 */
+    OR,           /* |                 */
+    PLUS_CLOSE,   /* +                 */
+} TOKEN;
+
+TOKEN Tokmap[] = 
+{
+/*  ^@  ^A  ^B  ^C  ^D  ^E  ^F  ^G  ^H  ^I  ^J  ^K  ^L  ^M  ^N   */
+    L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L, 
+
+/*  ^O  ^P  ^Q  ^R  ^S  ^T  ^U  ^V  ^W  ^X  ^Y  ^Z  ^[  ^\  ^]   */
+    L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L, 
+
+/*  ^^  ^_  SPACE   !   "   #   $       %   &   , */
+    L,  L,  L,      L,  L,  L,  AT_EOL, L,  L,  L,
+
+/*  (           )            *        +           ,  -     .    */
+    OPEN_PAREN, CLOSE_PAREN, CLOSURE, PLUS_CLOSE, L, DASH, ANY,
+
+/*  /   0   1   2   3   4   5   6   7   8   9   :   ;   <   =   */
+    L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L, 
+
+/*  >   ?                                                       */
+    L,  OPTIONAL,
+
+/*  @   A   B   C   D   E   F   G   H   I   J   K   L   M   N   */
+    L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L, 
+
+/*  O   P   Q   R   S   T   U   V   W   X   Y   Z               */
+    L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,
+
+/*  [          \  ]        ^                                    */
+    CCL_START, L, CCL_END, AT_BOL,
+
+/*  _   `   a   b   c   d   e   f   g   h   i   j   k   l   m   */
+    L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L, 
+
+/*  n   o   p   q   r   s   t   u   v   w   x   y   z           */
+    L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,  L,
+
+/*  {           |   }            DEL                            */
+    OPEN_CURLY, OR, CLOSE_CURLY, L,
+};
+
+static char *(*Ifunc)();    /* Input function pointer */
+static char *Input = "";    /* current position in input string */
+static char *S_input;       /* Beginning of input string */
+static TOKEN Current_tok;   /* Current token */
+static int Lexeme;          /* Value associated with LITERAL */
+
+#define MATCH(t) (Current_tok == (t))
+
+/*-----------------------------------------------------------------------------
+ * Lexical analyzer:
+ *
+ * Lexical analysis is trivial because all lexemes are single-character
+ * values. The only complications are escape sequences and quoted strings,
+ * both of which are handled by advance(), below. This routine advances past
+ * the current token, putting the new token into Current_tok and the
+ * equivalent lexeme into Lexeme. If the character was escaped, Lexeme holds
+ * the actual value. For example, if a '\s' is encountered, Lexeme will hold a
+ * space character. The MATCH(x) macro returns true if x matches the current
+ * token. Advance both modifiers Current_tok to the current token and return
+ * it. 
+ */
+static int advance(void)
+{
+    static int inquote = 0;    /* Processing quoted string */
+    int saw_esc;               /* saw a backslash '\'      */
+    static char *stack[SSIZE]; /* input-source stack       */
+    static char **sp = NULL;   /* stack pointer            */
+
+    if (sp == NULL) {
+        /* initialize sp. Necessary for large model */
+        sp = stack - 1;
+    }
+
+    /* Get another line */
+    if (Current_tok == EOS) {
+        if (inquote) {
+            parse_err(E_NEWLINE);
+        }
+
+        do {
+            Input = Ifunc();
+            if (Input == NULL) {
+                Current_tok = END_OF_INPUT;
+                goto exit;
+            }
+
+            /* ignoring leading space... and blank lines */
+            while (isspace(*Input)) {
+                Input ++;
+            }
+        } while ((*Input) == '\0');
+
+        S_input = Input;    /* Remember start of line for error messages. */
+    }
+
+    while (*Input == '\0') {
+        /* Restore previous input source */
+        if (INBOUNDS(stack, sp)) {
+            INput = *sp--;
+            continue;
+        }
+
+        Current_tok = EOS;  /* No more input sources to restore */
+        Lexeme = '\0';      /* i.e. you're at the real end of string */
+        goto exit;
+    }
+
+    if (!inquote) {
+        while (*Input == '{') {
+            /* Macro expansion required 
+             * Stack current input string adn replace it with the macro body */
+            *++sp = Input;
+            Input = expand_macro(sp);
+
+            if (TOOHIGH(stack, sp)) {
+                parse_err(E_MACDEPTH);  /* stack overflow */
+            }
+        }
+    }
+
+    /* At either start and end of a quoted string. All characters are treated
+     * as literals while inquote is true */
+    if (*Input == '"') {
+        inquote = ~inquote;
+        if (! *++Input) {
+            Current_tok = EOS;
+            Lexeme = '\0';
+            goto exit;
+        }
+    }
+
+    saw_esc = (*Input == '\\');
+
+    if (!inquote) {
+        if (isspace(*Input)) {
+            Current_tok = EOS;
+            Lexeme = '\0';
+            goto exit;
+        }
+        Lexeme = esc(&input);
+    } else {
+        if (saw_esc && Input[1] = '"') {
+            Input += 2;
+            Lexeme = '"';
+        } else {
+            Lexeme = *Input ++;
+        }
+    }
+
+    Current_tok = (inquote || saw_esc) ? L : Tokmap[Lexeme];
+
+exit:
+    return Current_tok;
+}
